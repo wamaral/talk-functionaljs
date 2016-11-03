55cba74ddcd07eb336e80932723b7605359a76e0
Wagner Amaral <wamaral@bionexo.com>
2016-11-03 20:58:04 -0200

    Change directions :)

---
+++ public/samples/data.js
@@ -0,0 +1,55 @@
+const products = [
+  { id: 1, name: "Dipirona", packing: "caixa" },
+  { id: 2, name: "AAS", packing: "caixa" },
+  { id: 3, name: "Advil", packing: "frasco" },
+  { id: 4, name: "Salonpas", packing: "caixa" },
+  { id: 5, name: "Cepacol", packing: "frasco" },
+  { id: 6, name: "Atroveran", packing: "ampola" },
+];
+
+const suppliers = [
+  { name: "Nestlé",
+    catalogue: [
+      { id: 1, price: 10, quantity: 500 },
+      { id: 2, price: 5, quantity: 20 },
+      { id: 3, price: 120, quantity: 120 },
+      { id: 4, price: 1, quantity: 5 },
+      { id: 5, price: 10, quantity: 25 },
+    ],
+  },
+  { name: "Microsoft",
+    catalogue: [
+      { id: 1, price: 9, quantity: 50 },
+      { id: 6, price: 25, quantity: 200 },
+    ],
+  },
+  { name: "Apple",
+    catalogue: [
+      { id: 1, price: 10, quantity: 50 },
+      { id: 2, price: 6, quantity: 50 },
+      { id: 4, price: 3, quantity: 100 },
+    ],
+  },
+  { name: "Google",
+    catalogue: [
+      { id: 2, price: 6, quantity: 200 },
+      { id: 3, price: 130, quantity: 20 },
+      { id: 5, price: 8, quantity: 1 },
+      { id: 6, price: 23, quantity: 300 },
+    ],
+  },
+  { name: "Mozilla",
+    catalogue: [
+      { id: 3, price: 125, quantity: 10 },
+      { id: 4, price: 3, quantity: 50 },
+    ],
+  },
+  { name: "Samsung",
+    catalogue: [
+      { id: 1, price: 10, quantity: 20 },
+      { id: 3, price: 120, quantity: 8 },
+      { id: 4, price: 2, quantity: 300 },
+      { id: 6, price: 25, quantity: 140 },
+    ],
+  },
+];
